6bf2ce51abfcc0b35f1ec64a46e1b36c8a8287c9
Akira Kozakai <akiraak@gmail.com>
2018-07-09 10:36:09 -0700

    Modity deploying message

--- deploy_bin.sh
+++ deploy_bin.sh
@@ -5,11 +5,11 @@ BIN_PATH="~/go/src/github.com/akiraak/go-manga"
 
 go build server.go
 scp server gmanganow:$BIN_PATH/server_new
-echo Built server
+echo Deploye server
 
 go build update_books.go
 scp update_books gmanganow:$BIN_PATH/update_books_new
-echo Built update_books
+echo Deploye update_books
 
 go build mailfile.go
 scp mailfile gmanganow:$BIN_PATH/mailfile_new
